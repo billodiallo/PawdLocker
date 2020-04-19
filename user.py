85195fe72e3a484d06afee05ea7471e1e07600ad
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-19 16:58:38 +0300

    adding new instance for user

--- user.py
+++ user.py
@@ -4,3 +4,9 @@ class User:
     """
     pass
 users_array = []
+
+def __init__(self, first_name, last_name, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
