f55a70106e884cf406506a48c7a6f48057c4213b
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-19 18:29:51 +0300

    update

--- credential.py
+++ credential.py
@@ -1,25 +1,28 @@
-class credential:
+class User:
     """
-    A class that generate new instance of credentials
+    a class that generates new instances of users
     """
     pass
-credential_array = []
 
-def __init__(self, user_name, password, email):
-        self.user_name = user_name
-        self.password = password
+    users_array = []
+
+    def __init__(self, first_name, last_name, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
         self.email = email
 
-        def save_credential(self):
+    def save_user_details(self):
         """
-        save_contact method saves credentials objects into credential_array
+        save_contact method saves contact objects into user_array
         """
-        Credential.credential_array.append(self)
-        
-@classmethod
-    def display_credential(cls):
+        User.users_array.append(self)
+
+    @classmethod
+    def display_users(cls):
         """
-        method that returns the credential array
+        method that returns the class array
         """
-        return cls.credential_array
+        return cls.users_array
+
 
--- user.py
+++ user.py
@@ -3,20 +3,20 @@ class User:
     a class that generates new instances of users
     """
     pass
-users_array = []
 
-def __init__(self, first_name, last_name, phone_number, email):
+    users_array = []
+
+    def __init__(self, first_name, last_name, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
 
-def save_user_details(self):
+    def save_user_details(self):
         """
         save_contact method saves contact objects into user_array
         """
         User.users_array.append(self)
-
 
     @classmethod
     def display_users(cls):
@@ -24,3 +24,5 @@ def save_user_details(self):
         method that returns the class array
         """
         return cls.users_array
+
+
