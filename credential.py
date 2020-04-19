377f43591eed0380a8ecd664e0d4a3c11fa04f36
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-04-19 17:54:13 +0300

    adding save credential

--- credential.py
+++ credential.py
@@ -9,3 +9,10 @@ def __init__(self, user_name, password, email):
         self.user_name = user_name
         self.password = password
         self.email = email
+
+        def save_credential(self):
+        """
+        save_contact method saves credentials objects into credential_array
+        """
+        Credential.credential_array.append(self)
+
